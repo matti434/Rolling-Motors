d3c4fb924d3c62bcc1dd9bd06238959bb232f01a
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-12-31 20:25:39 -0300

    feat: add modalLogin.jsx base

--- src/modals/modalLogin.jsx
+++ src/modals/modalLogin.jsx
@@ -2,9 +2,9 @@ import ModalBase from "../shared/ModalBase"
 
 export default function modalLogin({isOpen , onClose}){
   return (
-    <ModalBase  isOpen={isOpen} onClose={onClose}>
-     <h2>Login</h2>
-     
+    <ModalBase isOpen={isOpen} onClose={onClose} title="Iniciar Sesión">
+      <LoginForm onSuccess={onClose} /> 
+      {/* onSuccess cierra el modal luego de logear */}
     </ModalBase>
   )
 }
